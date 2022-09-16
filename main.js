2b7bf217dd4f2be1aed4140c558a2f9f95141496
franjaluga <playaimpuestos@gmail.com>
2022-09-16 14:10:32 -0300

    ahora genera tablas

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ excelInput.addEventListener('change', async function(){
     for(let i=0; i < content.length; i++){
         GenArr.push(content[i]);
     };
-
     console.log(GenArr);
-    
+    new gridjs.Grid({
+        columns: ["DATA1", "DATA2", "DATA3"],
+        data: GenArr
+      }).render(document.getElementById("wrapper"));
 })
+
